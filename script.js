4c6daaae432b33086e6a73f510d3081e9e0afc0b
oryam <16582185+oryam@users.noreply.github.com>
2025-12-19 08:57:01 +0100

    affichage du prenom

--- script.js
+++ script.js
@@ -2,5 +2,6 @@ console.log("Application Student Helper chargée");
 document.getElementById("app").innerHTML +=
   "<p>Application prête à être utilisée.</p>";
 document.getElementById("btnOk").addEventListener("click", function () {
-  console.log("Bouton cliqué");
+  const prenom = document.getElementById("prenom").value;
+  document.getElementById("app").innerHTML += `<p>Bonjour ${prenom} !</p>`;
 });
