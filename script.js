57fe5d086bbe175286fd4d2be0a8fb1aa1e7266b
oryam <16582185+oryam@users.noreply.github.com>
2025-12-19 08:49:23 +0100

    Ajout d'un evenement sur le bouton

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 console.log("Application Student Helper chargée");
 document.getElementById("app").innerHTML +=
   "<p>Application prête à être utilisée.</p>";
+document.getElementById("btnOk").addEventListener("click", function () {
+  console.log("Bouton cliqué");
+});
