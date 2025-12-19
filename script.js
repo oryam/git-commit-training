21e7d3e001703c23cd3602d4e008acbbe8d90738
oryam <16582185+oryam@users.noreply.github.com>
2025-12-19 09:02:34 +0100

    controle du champ input

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ console.log("Application Student Helper chargée");
 document.getElementById("app").innerHTML +=
   "<p>Application prête à être utilisée.</p>";
 document.getElementById("btnOk").addEventListener("click", function () {
+  if (prenom === "") {
+  alert("Veuillez entrer un prénom");
+  return;
+}
   const prenom = document.getElementById("prenom").value;
   document.getElementById("app").innerHTML += `<p>Bonjour ${prenom} !</p>`;
 });
